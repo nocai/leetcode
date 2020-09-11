a916d7f26ff5267b7a844aecd5b549e2b7da645c
nocai <395822362@qq.com>
2020-09-11 17:45:25 +0800

    linked list

---
+++ linked-list/141.linked-list-cycle_test.go
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=golang
+ *
+ * [141] 环形链表
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (49.27%)
+ * Total Accepted:    225.6K
+ * Total Submissions: 457.9K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表，判断链表中是否有环。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ *
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ *
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *
+ *
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ *
+ */
+
+func hasCycle(head *ListNode) bool {
+	return hasCycle_slow_fast_pointers(head)
+}
+
+func hasCycle_two_pointers(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow, fast := head, head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
+
+func hasCycle_slow_fast_pointers(head *ListNode) bool {
+	m := map[*ListNode]struct{}{}
+	for head != nil {
+		if _, exist := m[head]; exist {
+			return true
+		} else {
+			m[head] = struct{}{}
+		}
+		head = head.Next
+	}
+	return false
+}
---
+++ linked-list/list_test.go
@@ -0,0 +1,105 @@
+package leetcode
+
+import "testing"
+
+func TestList(t *testing.T) {
+	// 一。在链表中间添加节点
+	// 1. cur := new(cur)
+	// 2. cur.Next = prev.Next
+	// 3. prev.Next = cur
+
+	// 二。在链表头添加节点
+	// 1. cur := new(cur) // 初始化一个新节点
+	// 2. cur.Next = head // 将新节点链接到我们原始的头结点head上
+	// 3. head = cur // 将cur指定为head
+
+	// 三。在链表中间删除节点
+	// 1. 找到prev, cur, next
+	// 2. prev.Next = cur.Next
+
+	// 四。在链表头删除
+	// 1. 简单地“将下一个结点分配给head” head = head.Next
+}
+
+type MyLinkedList struct{}
+
+func (this *MyLinkedList) Get(index int) int {
+	return 0
+}
+
+func (this *MyLinkedList) AddAtHead(val int) {
+
+}
+
+func (this *MyLinkedList) AddAdTail(val int) {
+
+}
+
+func (this *MyLinkedList) AddAtIndex(index int, val int) {
+
+}
+
+func (this *MyLinkedList) DeleteAdIndex(index int) {
+
+}
+
+// Definition for singly-linked list.
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
+
+// func (ln *ListNode) String() string {
+// 	var rst strings.Builder
+// 	rst.WriteString("[")
+// 	for ln != nil {
+// 		rst.WriteString(fmt.Sprintf("%v", ln.Val))
+// 		if ln.Next != nil {
+// 			rst.WriteRune(',')
+// 		}
+// 		ln = ln.Next
+// 	}
+// 	rst.WriteString("]")
+// 	return rst.String()
+// }
+
+// func InitLinkedList(vals ...int) *ListNode {
+// 	if len(vals) == 0 {
+// 		return nil
+// 	}
+
+// 	head := &ListNode{Val: vals[0]}
+// 	tail := head
+
+// 	for _, val := range vals[1:] {
+// 		cur := &ListNode{Val: val}
+// 		tail.Next = cur
+// 		tail = tail.Next
+// 	}
+// 	return head
+// }
+
+// func TestInitLinkedList(t *testing.T) {
+// 	t.Log(InitLinkedList(1, 2, 3))
+// }
+
+// // func hasCycle(head *ListNode) bool {
+
+// // 	return false
+// // }
+
+// func TestHasCycle_hash(t *testing.T) {
+// 	for index, tc := range []struct {
+// 		head     *ListNode
+// 		hasCycle bool
+// 	}{
+
+// 		{InitLinkedList(3, 2, 0, -4), true},
+// 		{InitLinkedList(1, 2), true},
+// 		{InitLinkedList(1), false},
+// 	} {
+// 		t.Run(fmt.Sprintf("case: %v", index), func(t *testing.T) {
+// 			// assert.Equal(t, tc.hasCycle, hasCycle_hash(tc.head))
+// 		})
+// 	}
+// }
